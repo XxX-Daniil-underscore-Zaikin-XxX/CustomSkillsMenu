2f52134b3e32a8546cc02fb934f3f6c26ca6321a
Daniil Zaikin <dan.zaikin@gmail.com>
2025-06-04 16:58:40 +1000

    makes hidden properly apply in all cases

--- dist/skse/Plugins/JCData/Lua/msm/init.lua
+++ dist/skse/Plugins/JCData/Lua/msm/init.lua
@@ -185,20 +185,18 @@ function msm.applyHidden(menus, hiddenOptions)
 end
 
 -- sets the hidden key of each menu item based on the hiddenoption
+-- we overwrite whatever's in the menu
 function msm.setHidden(menus, hiddenOptions)
     for menuName, hidden in pairs(hiddenOptions) do
-        if hidden == nil then
+        local menu = menus[menuName]
+        if menu == nil then
             goto continue
         end
-        local menu = menus[menuName]
-        local hiddenSwitch = hidden["hidden"]
-        if hiddenSwitch ~= nil then
-            if menu ~= nil then
-                menu["hidden"] = hiddenSwitch
-            end
+        if hidden and hidden["hidden"] ~= nil then
+            menu["hidden"] = hidden["hidden"]
         else
             menu["hidden"] = false
-        end
+        end   
         ::continue::
     end
     return menus
@@ -207,8 +205,8 @@ end
 -- transfer menus to hiddenOptions to preserve original mod's functionality
 function msm.transferToHidden(menus, hiddenOptions)
     for menuName, menu in pairs(menus) do
-        if hiddenOptions[menuName] == nil then
-            local newHidden = false
+        if hiddenOptions[menuName] == nil or hiddenOptions[menuName]["hidden"] == nil then
+            local newHidden = 0
             if menu["hidden"] ~= nil then
                 newHidden = menu["hidden"]
             end
